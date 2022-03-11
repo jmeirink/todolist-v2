988a023ff68a3ae19e865e39252a4034c4ba305f
jmeirink <jesse@meirink.com>
2022-03-11 15:56:33 -0600

    Added ability to add custom lists using Express route parameters

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+
+const List = mongoose.model("List", listSchema);
 
 
 app.get("/", function(req, res) {
@@ -52,6 +59,31 @@ app.get("/", function(req, res) {
       res.render("list", {listTitle: "Today", newListItems: foundItems});
     }
   });
+
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
+
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+      }
+    }
+  });
+
+
 
 });
 
@@ -75,11 +107,6 @@ app.post("/delete", function(req, res){
       res.redirect("/");
     }
   });
-});
-
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.get("/about", function(req, res){
